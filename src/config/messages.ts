cb37506eaeccde348ec918ac611265bd49d4e1dd
sudoarchie <shivamsinghcse21@gmail.com>
2026-03-02 14:34:49 +0530

    contant messages

---
+++ src/config/messages.ts
@@ -0,0 +1,7 @@
+export const success = {
+    REGISETERATION_SUCCESSFUL : "Registration Successful",
+    LOGIN_SUCCESSFUL : "Login Successful"
+}
+export const error = {
+    INTERNAL_SERVER_ERROR: "Internal Server Error",
+}
