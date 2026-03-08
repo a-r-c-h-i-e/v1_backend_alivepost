c92318607b812a6a480c051b0579b120ee401116
whyarchie <http.shivamsingh@gmail.com>
2026-03-08 12:51:30 +0530

    disease debounce service

--- src/features/disease/disease.controller.ts
+++ src/features/disease/disease.controller.ts
@@ -1,18 +1,32 @@
 import express from "express";
 import { diseaseSchema, type DiseaseCreateSchema } from "./disease.schema";
-import { CreateDisease } from "./disease.service";
+import { CreateDisease, DiseaseSearchService } from "./disease.service";
+
 const diseaseRouter = express.Router();
 
 diseaseRouter.post("/create", async (req, res, next) => {
-    try {
-    const data :DiseaseCreateSchema= req.body;
+  try {
+    const data: DiseaseCreateSchema = req.body;
     const safeData = diseaseSchema.parse(data);
-    const diseaseData = await CreateDisease(safeData)
+    const diseaseData = await CreateDisease(safeData);
     res.status(201).json({
-        success:true,
-        data: diseaseData 
-    })
-    }catch (error) {
-     next(error)   
-    }})
+      success: true,
+      data: diseaseData,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+diseaseRouter.get("/search", async (req, res, next) => {
+  try {
+    const value = req.query.value as string;
+    const output =await DiseaseSearchService(value);
+    res.status(200).json({
+      success: true,
+      data: output,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 export default diseaseRouter;
--- src/features/disease/disease.service.ts
+++ src/features/disease/disease.service.ts
@@ -1,4 +1,5 @@
 import prisma from "../../config/prisma";
+import { AppError } from "../../utils/AppError";
 import type { DiseaseCreateSchema } from "./disease.schema";
 
 export async function CreateDisease(data: DiseaseCreateSchema) {
@@ -6,9 +7,27 @@ export async function CreateDisease(data: DiseaseCreateSchema) {
     data: {
       name: data.name,
       type: data.type,
-      description: data.description
-    }
+      description: data.description,
+    },
   });
 
   return disease;
+}
+
+export async function DiseaseSearchService(value: string) {
+  try {
+    const diseases = await prisma.disease.findMany({
+      where: {
+        name: {
+          contains: value,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    return diseases;
+  } catch (error) {
+
+    throw new AppError("Unexpected error while searching diseases", 500);
+  }
 }
