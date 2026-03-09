a213874d92d8dc2439626d5e59bcfc7371631d23
whyarchie <http.shivamsingh@gmail.com>
2026-03-09 19:29:44 +0530

    medicine search / debouncing api

--- src/features/medicine/medicine.controller.ts
+++ src/features/medicine/medicine.controller.ts
@@ -1,20 +1,34 @@
 import express from "express";
 import { MedicineSchema, type MedicineCreateSchema } from "./medicine.schema";
-import { CreateMedicine } from "./medicine.service";
+import { CreateMedicine, MedicineSearch } from "./medicine.service";
 
 const medicineRouter = express.Router();
-
+//Create Medicine
 medicineRouter.post("/create", async (req, res, next) => {
   try {
     const data: MedicineCreateSchema = req.body;
     const safeData = MedicineSchema.parse(data);
-    const medicine = await CreateMedicine(safeData)
+    const medicine = await CreateMedicine(safeData);
     res.status(200).json({
-      success:true,
-      data: medicine
-    })
+      success: true,
+      data: medicine,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
-export default medicineRouter
+
+//Search Medicine or for debouncing
+medicineRouter.get("/search", async (req, res, next) => {
+  try {
+    const value = req.query.value as string;
+    const medicine = await MedicineSearch(value);
+    res.status(200).json({
+      success: true,
+      data: medicine,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+export default medicineRouter;
--- src/features/medicine/medicine.service.ts
+++ src/features/medicine/medicine.service.ts
@@ -4,13 +4,25 @@ import type { MedicineCreateSchema } from "./medicine.schema";
 export async function CreateMedicine(data: MedicineCreateSchema) {
   try {
     const medicine = await prisma.medicine.create({
-      data
+      data,
     });
 
     return medicine;
-
   } catch (error) {
-    console.log(error)
+    console.log(error);
     throw error;
   }
+}
+
+export async function MedicineSearch(value: string) {
+  const medicine = await prisma.medicine.findMany({
+    where: {
+      OR: [
+        { brandName: { contains: value, mode: "insensitive" } },
+        { genericName: { contains: value, mode: "insensitive" } },
+        { manufacturer: { contains: value, mode: "insensitive" } },
+      ],
+    },
+  });
+  return medicine;
 }
